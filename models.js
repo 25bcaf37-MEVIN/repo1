e1f662c84287c3f7bbe1dbd498dcf340e7a314b9
25bcaf37-MEVIN <25bcaf37@kristujayanti.com>
2026-03-22 18:46:50 +0530

    Add Todo model with schema definition

---
+++ models.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+const TodoSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  text: String,
+  completed: { type: Boolean, default: false }
+});
+export default mongoose.model("Todo", TodoSchema);
